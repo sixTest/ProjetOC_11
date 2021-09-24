e1f1e2821da7225e379a91434923f30dc2cfe7eb
Quentin Hors <hors.quentin@outlook.fr>
2021-09-24 16:37:27 +0200

    Creation of an exception if a club tries to book more places than his points

--- exceptions.py
+++ exceptions.py
@@ -5,3 +5,11 @@ class EmailNotFound(Exception):
 
     def __str__(self):
         return self.msg
+
+
+class NotEnoughPoints(Exception):
+    def __init__(self):
+        self.msg = "Sorry the club doesn't have enough points"
+
+    def __str__(self):
+        return self.msg
