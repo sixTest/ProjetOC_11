0e1f665d5e96117e24166c9e4d94f6102d45d8f7
Quentin Hors <hors.quentin@outlook.fr>
2021-09-24 17:12:02 +0200

    Creation of an exception if a club tries to book more places than competition places

--- exceptions.py
+++ exceptions.py
@@ -7,9 +7,18 @@ class EmailNotFound(Exception):
         return self.msg
 
 
-class NotEnoughPoints(Exception):
+class ClubNotEnoughPoints(Exception):
     def __init__(self):
-        self.msg = "Sorry the club doesn't have enough points"
+        self.msg = "Sorry the club doesn't have enough points."
 
     def __str__(self):
         return self.msg
+
+
+class CompetitionNotEnoughPlaces(Exception):
+    def __init__(self):
+        self.msg = "Sorry this competition doesn't have enough places."
+
+    def __str__(self):
+        return self.msg
+
